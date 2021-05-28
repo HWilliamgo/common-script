ca4580221a1c5a1b4fc6ba43b6d6a7f829c96a6f
HWilliamGo <williamintoandroid@outlook.com>
2021-05-28 09:44:24 +0800

    能够读取软连接

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,7 @@
 echo "欢迎进入脚本启动器"
 # 获取当前脚本的绝对路径
-SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
-echo "当前脚本路径=$SHELL_FOLDER\n"
+SHELL_FOLDER=$(dirname $(readlink "$0"))
+echo "当前脚本真实路径=$SHELL_FOLDER\n"
 
 # kotlinc -script $SHELL_FOLDER/kotlinScript/projectZip.kts
 chmod +x $SHELL_FOLDER/kotlinScript/projectZip.main.kts
